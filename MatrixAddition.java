285f8bc2df7e7b2034affdcdefa6577e9bc17112
Abhishek Rai <abhishekrai.apj@gmail.com>
2022-09-11 13:28:23 -0700

    Add files via upload

---
+++ MatrixAddition.java
@@ -0,0 +1,33 @@
+public class MatrixAddition {
+    
+    static void checkAndAddMatrices(int[][] a, int[][] b) {
+        if(!(a.length == b.length) || !(a[0].length == b[0].length))
+            System.out.println("Matrices differ in size, hence cannot be added!");
+        else {
+            int[][] res = new int[a.length][a[0].length];
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a[0].length; j++) {
+                    res[i][j] = a[i][j] + b[i][j];
+                    System.out.print(res[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        MatrixAddition matrix = new MatrixAddition();
+        int[][] a = {
+            {2, 5, 4, 9},
+            {0, 9, 6, 3},
+            {1, 1, 4, 7}
+        };
+        int[][] b = {
+            {1, 2, 3, 4},
+            {8, 7, 6, 5},
+            {0, 1, 2, 3}
+        };
+        checkAndAddMatrices(a, b);
+    }
+    
+}
