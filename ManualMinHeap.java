10e1a9e9eda7dd78af3b61589ab207779c2a16c8
Abhishek Rai <abhishekrai.apj@gmail.com>
2024-03-30 14:00:45 -0700

    Update ManualMinHeap.java

--- ManualMinHeap.java
+++ ManualMinHeap.java
@@ -1,107 +1,105 @@
-/*
- * Click `Run` to execute the snippet below!
- */
-
-import java.io.*;
 import java.util.*;
 
-/*
- * To execute Java, please define "static void main" on a class
- * named Solution.
- *
- * If you need more classes, simply define them inline.
- */
+class Solution {
 
-class MinHeap {
+  public static void main(String[] args) {
+    //good video to understand heapify operations: https://www.youtube.com/watch?v=Dvq-YKeuO9Y&ab_channel=BlueTreeCode
+    //Create a Min Heap structure
+    //perform 3 operations - add, poll, peek
+    MinHeap minHeap = new MinHeap(7);
+    minHeap.add(4);
+    System.out.println(minHeap.peek()); //4
+    minHeap.add(-4);
+    minHeap.add(0);
+    minHeap.add(-1);
+    System.out.println(minHeap.peek()); //-4
+    minHeap.add(3);
+    minHeap.add(-1);
+    minHeap.add(1);
+    System.out.println(minHeap.peek()); //-4
+    //capacity reached, next insertions should remove min element from heap and add new element
+    System.out.println(Arrays.toString(minHeap.getArray())); //-4,-1,-1,0,1,3,4
+    minHeap.add(2);
+    System.out.println(minHeap.peek()); //-1
+    System.out.println(Arrays.toString(minHeap.getArray())); //-1,-1,0,1,2,3,4
+    System.out.println(minHeap.poll()); //-1
+    System.out.println(Arrays.toString(minHeap.getArray())); //-1,0,1,2,3,4
+    System.out.println(minHeap.poll()); //-1
+    System.out.println(Arrays.toString(minHeap.getArray())); //0,1,2,3,4
+    minHeap.add(-2);
+    System.out.println(minHeap.peek()); //-2
+    System.out.println(Arrays.toString(minHeap.getArray())); //-2,0,1,2,3,4
+  }
   
-  int size;
-  int capacity;
-  int[] heap;
-
-  MinHeap(int capacity) {
-    this.size = 0;
-    this.capacity = capacity;
-    this.heap = new int[capacity];
-  }
-
-  void insert(int value) {
-    if (size == capacity) {
-      System.out.println("Capacity reached! Cannot insert!");
-    } else {
-      heap[size] = value;
-      size++;
-      //bring the new element to its correct position by heapifying
-      heapUp(size-1);
-    }
-  }
-
-  int getMin() {
-    if (size == 0) {
-      System.out.println("Empty heap, no element can be returned!");
-    }
-    int val = heap[0];
-    heap[0] = heap[size-1];
-    size--;
-    heapDown(0);
-    return val;
-  }
-
-  void heapUp(int index) {
-    int parentIndex = (index-1)/2;
-    while (index > 0 && heap[parentIndex] > heap[index]) {
-      swap(index, parentIndex);
-      index = parentIndex;
-      parentIndex = (index-1)/2;
-    }
-  }
-
-  void heapDown(int index) {
-    int leftChildIndex = index*2 + 1;
-    int rightChildIndex = index*2 + 2;
-    int smallest = index;
-    if (leftChildIndex < size && heap[leftChildIndex] < heap[smallest]) {
-      smallest = leftChildIndex;
-    }
-    if (rightChildIndex < size && heap[rightChildIndex] < heap[smallest]) {
-      smallest = rightChildIndex;
-    }
-    //if any of children are smaller, then swap and keep heapifying
-    if (smallest != index) {
-      swap(index, smallest);
-      index = smallest;
-      heapDown(index);
-    }
-  }
-
-  void swap(int ind1, int ind2) {
-      int temp = heap[ind1];
-      heap[ind1] = heap[ind2];
-      heap[ind2] = temp;
-    }
 }
 
-class Solution {
-  public static void main(String[] args) {
-    ArrayList<String> strings = new ArrayList<String>();
-    strings.add("Hello, World!");
-    strings.add("Welcome to CoderPad.");
-    strings.add("This pad is running Java " + Runtime.version().feature());
+class MinHeap {
+    
+    int[] heap;
+    int size;
+    int capacity;
 
-    for (String string : strings) {
-      System.out.println(string);
+    MinHeap(int capacity) {
+        this.capacity = capacity;
+        this.size = 0;
+        this.heap = new int[capacity];
     }
 
-    MinHeap minHeap = new MinHeap(5);
-    minHeap.insert(7);
-    minHeap.insert(4);
-    minHeap.insert(9);
-    minHeap.insert(1);
-    minHeap.insert(2);
-    minHeap.insert(6);
-    minHeap.insert(8);
-    System.out.println("Current min: " + minHeap.getMin());
-    System.out.println("Current min: " + minHeap.getMin());
-    System.out.println("Current min: " + minHeap.getMin());
-  }
-  
+    public void add(int val) {
+        if (size == capacity)
+            poll();
+        size++;
+        heap[size-1] = val;
+        heapUp(size-1);
+    }
+
+    public void heapUp(int index) {
+        while (index > 0) {
+            int parentIndex = (index-1)/2;
+            if (heap[index] < heap[parentIndex])
+                swap(index, parentIndex);
+            index = parentIndex;
+        }
+    }
+
+    public void swap(int i, int j) {
+        int temp = heap[i];
+        heap[i] = heap[j];
+        heap[j] = temp;
+    }
+
+    public int poll() {
+        int val = heap[0];
+        swap(0, size-1);
+        size--;
+        heapDown(0);
+        return val;
+    }
+
+    public void heapDown(int index) {
+        if (index == size-1)
+            return;
+        int leftChildIndex = index*2 + 1;
+        int rightChildIndex = index*2 + 2;
+        int smallest = index;
+        if (leftChildIndex < size && heap[leftChildIndex] < heap[smallest])
+            smallest = leftChildIndex;
+        if (rightChildIndex < size && heap[rightChildIndex] < heap[smallest])
+            smallest = rightChildIndex;
+        if (smallest != index) {
+            swap(index, smallest);
+            heapDown(smallest);
+        }
+    }
+
+    public int peek() {
+        return heap[0];
+    }
+
+    //this method only returns the heap array, which doesnt actually represent the sorted array
+    //it just represents the array that the heap uses to maintain its sorted structure
+    public int[] getArray() {
+        return heap;
+    }
+
 }
