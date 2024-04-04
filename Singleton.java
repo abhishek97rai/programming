388c471c488815aa454819a982dd526e5417b985
Abhishek Rai <abhishekrai.apj@gmail.com>
2024-04-03 19:30:07 -0700

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,36 @@
+/*
+ * Click `Run` to execute the snippet below!
+ */
+
+import java.io.*;
+import java.util.*;
+
+/*
+ * To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+
+class Solution {
+  public static void main(String[] args) {
+        System.out.println(string);
+  }
+}
+
+class Singleton {
+    private static Singleton instance;
+
+    //protected so that it can be used by sub classes
+    protected Singleton() {
+
+    }
+
+    //static so that we can directly call Singleton.getInstance(), like Logger.getLogger()
+    public static Singleton getInstance() {
+        if (instance == null) {
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
